8354c94ee9111281efb1c14c1fe6a78170dac400
adrianneeeee <168504582+adrianneeeee@users.noreply.github.com>
2025-11-03 05:21:28 +0000

    Flutter 3.32.5 V Firebase Integration Part 1

---
+++ lazundin - ecommerce/ecommerce_app/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() async {
+
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+  FlutterNativeSplash.remove();
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'eCommerce App',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My E-Commerce App'),
+        ),
+        body: Center(
+          child: Text('Firebase is Connected!'),
+        ),
+      ),
+    );
+  }
+}
+
+
